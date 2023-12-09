163ec544de0c382544821cff3930c5579ca411aa
Sameer Kushwaha <sameerkushwaha@Sameers-MacBook-Air.local>
2023-12-10 01:49:40 +0530

    working network switcher

--- client/src/pages/NetworkSelector.tsx
+++ client/src/pages/NetworkSelector.tsx
@@ -1,17 +1,27 @@
 import {Layout} from "../components/Layout";
-import {Avatar, List, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
+import {Avatar, ListItem, ListItemAvatar, ListItemText} from "@mui/material";
 import Button from "@mui/material/Button";
 import FolderIcon from '@mui/icons-material/Folder';
 import Typography from "@mui/material/Typography";
 import Grid2 from "@mui/material/Unstable_Grid2";
+import chainConfig from "../onboarding/config.json";
+import {switchNetwork} from "../onboarding/onboard";
+import {UIState, useGlobalContext} from "../contextProviders/GlobalContextProvider";
+import {useEffect, useState} from "react";
 
 interface INetworkSelectorProps {}
 
-const supportedNetworksConfig = [
-    {name: "A", logo: "a"},
-    {name: "B", logo: "b"},
-    {name: "C", logo: "c"},
-]
+interface INetworksConfig {
+    "chainId": string,
+    "chainName": string,
+    "symbol": string,
+    "decimals": number,
+    "rpcUrls": string[],
+    "blockExplorerUrls": string[],
+    "minimumWalletBalance": number
+}
+
+const supportedNetworksConfig: INetworksConfig[] = chainConfig;
 
 export const NetworkSelector = (props: INetworkSelectorProps) => {
     return <Layout>
@@ -19,32 +29,44 @@ export const NetworkSelector = (props: INetworkSelectorProps) => {
             List of supported networks
         </Typography>
         <Grid2 xs display="flex" justifyContent="center" alignItems="center" flexDirection={"column"}>
-                <ListItem
-                    secondaryAction={
-                        <Button variant="outlined">Connect</Button>
-                    }
-
-                >
-                    <ListItemAvatar>
-                        <Avatar>
-                            <FolderIcon />
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Sample" />
-                </ListItem>
-                <ListItem
-                    secondaryAction={
-                        <Button variant="outlined">Outlined</Button>
-                    }
-                >
-                    <ListItemAvatar>
-                        <Avatar>
-                            <FolderIcon />
-                        </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText primary="Sample" />
-                </ListItem>,
-            {/*</List>*/}
+            <NetworkDetailsList networksConfig={supportedNetworksConfig}/>
         </Grid2>
     </Layout>
+}
+
+const NetworkDetailsList = ({networksConfig}: {networksConfig: INetworksConfig[]}) => {
+    return <>
+        {networksConfig.map((config, i) => <NetworkDetailsItem {...config} key={i}/>)}
+    </>
+}
+
+const NetworkDetailsItem = (networkConfig: INetworksConfig) => {
+    const {chainId, setUIState} = useGlobalContext();
+    const [isConnected, setIsConnected] = useState<boolean>(false);
+
+    useEffect(() => {
+        setIsConnected(chainId == networkConfig.chainId);
+    }, [chainId]);
+
+    const networkSwitchHandler = async () => {
+        await switchNetwork(networkConfig.chainId)
+        console.log("switchind")
+        setUIState(UIState.DISPLAY_DASHBOARD);
+    }
+
+    return <ListItem
+        secondaryAction={
+            isConnected ?
+                <Button variant="contained" color="success" onClick={() => {}}>Connected</Button>
+                :
+                <Button variant="outlined" color="info" onClick={networkSwitchHandler}>Switch</Button>
+        }
+    >
+        <ListItemAvatar>
+            <Avatar>
+                <FolderIcon />
+            </Avatar>
+        </ListItemAvatar>
+        <ListItemText primary={networkConfig.chainName} />
+    </ListItem>
 }
