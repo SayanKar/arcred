cbde3f3b3e7fc5b6887f09cfcc50ecb58bd12b09
Sameer Kushwaha <sameerkushwaha@Sameers-MacBook-Air.local>
2023-12-10 06:40:13 +0530

    fetch credit report via lender

--- client/src/components/LenderDashboard.tsx
+++ client/src/components/LenderDashboard.tsx
@@ -1,20 +1,28 @@
 
 import Grid2 from "@mui/material/Unstable_Grid2";
 import Typography from "@mui/material/Typography";
-import {FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField} from "@mui/material";
+import {Box, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField} from "@mui/material";
 import Button from "@mui/material/Button";
 import * as React from "react";
-import {useEffect, useRef, useState} from "react";
-import {BorrowerStats, LoanData, useApiContext} from "../contextProviders/APIContextProvider";
+import {useRef, useState} from "react";
+import {BorrowerStats, useApiContext} from "../contextProviders/APIContextProvider";
 import {useGlobalContext} from "../contextProviders/GlobalContextProvider";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
 
 
 export const LenderDashboard = () => {
 
     const {account} = useGlobalContext();
-    const {registerLoan} = useApiContext();
+    const {registerLoan, getBorrowerCreditReport} = useApiContext();
 
     const lendingFormData = useRef({loanType: 0, description: '', amount: 0, borrowerAddress: ''})
+    const [borrowerAddress, setBorrowerAddress] = useState('');
+    const [borrowerCreditReport, setBorrowerCreditReport] = useState<BorrowerStats>({} as BorrowerStats)
 
     const handleLendingRegistration = () => {
         console.log(lendingFormData.current)
@@ -23,6 +31,15 @@ export const LenderDashboard = () => {
                 if(res.isError){
                     alert(res.message);
                 }
+        })
+    }
+
+    const fetchBorrowerCreditReport = () => {
+        getBorrowerCreditReport(borrowerAddress).then(res => {
+            if(res.isError){
+                alert(res.message);
+            }
+            setBorrowerCreditReport(res.item!);
         })
     }
 
@@ -55,5 +72,59 @@ export const LenderDashboard = () => {
         </Typography>
         <Grid2 xs display="flex" justifyContent="center" alignItems="center" flexDirection={"column"}>
         </Grid2>
+
+
+        <Box sx={{marginRight: "50px"}}>
+            <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div" marginTop={2}>
+                Get borrower's credit report
+            </Typography>
+
+            <Grid2 xs >
+                <TextField sx={{ width: '500px' }} id="borrower's account id" label="Borrower Address" variant="outlined" onChange={(e) => { setBorrowerAddress(e.target.value) }} />
+                <br /><br />
+                <Button color="success" variant="outlined" onClick={fetchBorrowerCreditReport}>Fetch credit report</Button>
+            </Grid2>
+        </Box>
+
+        {(borrowerCreditReport?.creditScore && borrowerCreditReport?.creditScore !== 0) && <Grid2 xs sx={{width: '700px'}} display="flex" justifyContent="center" alignItems="center"
+                flexDirection={"column"}>
+            {
+                getDataTable([
+                    {
+                        attrName: 'Credit Score',
+                        val: borrowerCreditReport.creditScore?.toString(),
+                    },
+                    {
+                        attrName: 'Number of Defaults',
+                        val: borrowerCreditReport.numberOfDefaults?.toString(),
+                    },
+                    {
+                        attrName: 'Number of Consumer Loans',
+                        val: borrowerCreditReport?.numberOfConsumerLoans?.toString(),
+                    },
+                    {
+                        attrName: 'Number of Credit Lines',
+                        val: borrowerCreditReport?.numberOfCreditLines?.toString(),
+                    }
+                ])
+            }
+        </Grid2>}
     </>
+}
+
+const getDataTable = (data: { attrName: string, val: string }[]) => {
+
+    return <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableBody>
+                {data.map((d, i) => <TableRow
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                    <TableCell component="th" scope="row">{d.attrName}</TableCell>
+                    <TableCell align="right">{d.val}</TableCell>
+                </TableRow>)
+                }
+            </TableBody>
+        </Table>
+    </TableContainer>
 }
