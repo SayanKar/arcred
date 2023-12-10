19c0eca55701b4197a8d5affb4674bf60ef2c145
Sayan Kar <sayankar1308@gmail.com>
2023-12-10 05:50:35 +0530

    minor fix

--- client/src/components/BorrowerDashboard.tsx
+++ client/src/components/BorrowerDashboard.tsx
@@ -96,50 +96,52 @@ export const BorrowerDashboard = () => {
     }
 
     return <>
-        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div" marginTop={2}>
-            Your credit report
-        </Typography>
-        <Grid2 xs sx={{ width: '700px' }} display="flex" justifyContent="center" alignItems="center" flexDirection={"column"}>
-            {
-                getDataTable([
+        <Box sx={{display:"flex", justifyContent: "space-between"}}>
+            <Box>
+                <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div" marginTop={2}>
+                    Your credit report
+                </Typography>
+                <Grid2 xs sx={{ width: '700px' }} display="flex" justifyContent="center" alignItems="center" flexDirection={"column"}>
                     {
-                        attrName: 'Credit Score',
-                        val: creditReport.creditScore?.toString(),
-                    },
-                    {
-                        attrName: 'Number of Defaults',
-                        val: creditReport.numberOfDefaults?.toString(),
-                    },
-                    {
-                        attrName: 'Number of Consumer Loans',
-                        val: creditReport?.numberOfConsumerLoans?.toString(),
-                    },
-                    {
-                        attrName: 'Number of Credit Lines',
-                        val: creditReport?.numberOfCreditLines?.toString(),
+                        getDataTable([
+                            {
+                                attrName: 'Credit Score',
+                                val: creditReport.creditScore?.toString(),
+                            },
+                            {
+                                attrName: 'Number of Defaults',
+                                val: creditReport.numberOfDefaults?.toString(),
+                            },
+                            {
+                                attrName: 'Number of Consumer Loans',
+                                val: creditReport?.numberOfConsumerLoans?.toString(),
+                            },
+                            {
+                                attrName: 'Number of Credit Lines',
+                                val: creditReport?.numberOfCreditLines?.toString(),
+                            }
+                        ])
                     }
-                ])
-            }
 
 
-        </Grid2>
+                </Grid2>
+            </Box>
+            <Box sx={{marginRight: "50px"}}>
+                <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div" marginTop={2}>
+                Approve lender to view your credit score and report your loans
+                </Typography>
 
+                <Grid2 xs >
+                    <TextField sx={{ width: '500px' }} id="lender's account id" label="Lender Address" variant="outlined" onChange={(e) => { setLenderAddress(e.target.value) }} />
+                    <br /><br />
+                    <Button color="success" variant="outlined" onClick={handleLenderAllowlist}>Allow</Button>
+                </Grid2></Box>
+        </Box>
         <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div" marginTop={2}>
             Your loan history
         </Typography>
         <Grid2 xs display="flex" justifyContent="center" alignItems="center" flexDirection={"column"}>
             {getLoanTable(loanData)}
-        </Grid2>
-
-
-        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div" marginTop={2}>
-            Whitelist a lender to view your credit score
-        </Typography>
-
-        <Grid2 xs >
-            <TextField sx={{ width: '500px' }} id="lender's account id" label="Lender's Account Id" variant="outlined" onChange={(e) => { setLenderAddress(e.target.value) }} />
-            <br /><br />
-            <Button color="success" variant="outlined" onClick={handleLenderAllowlist}>Allow</Button>
         </Grid2>
     </>
 }
@@ -167,40 +169,40 @@ const getLoanTable = (loanData: any) => {
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
                 <TableRow>
-                    <TableCell sx={{fontWeight: "600"}}>LoanID</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>Type</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>Description</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>CreationTime</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>SanctionedAmount</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>Lender</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>Borrower</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>IsActive</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>Unsettled Amount</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>Default Amount</TableCell>
-                    <TableCell align="right" sx={{fontWeight: "600"}}>Last updated</TableCell>
+                    <TableCell sx={{ fontWeight: "600" }}>LoanID</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>Type</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>Description</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>CreationTime</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>SanctionedAmount</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>Lender</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>Borrower</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>IsActive</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>Unsettled Amount</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>Default Amount</TableCell>
+                    <TableCell align="right" sx={{ fontWeight: "600" }}>Last updated</TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
-                {loanData.length === 0 ? 
-                <TableRow> 
-                    <Box sx={{width: "100%", height: "50px", display:"flex", justifyContent: "center", alignItems:"center"}}>No data to show</Box> 
-                </TableRow>:
-                loanData.map((d: any, i: number) => <TableRow
-                    key={i}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                    <TableCell>{d.loanId}</TableCell>
-                    <TableCell align="right">{d.type}</TableCell>
-                    <TableCell align="right">{d.desc}</TableCell>
-                    <TableCell align="right">{d.creationTime}</TableCell>
-                    <TableCell align="right">{d.sanctionedAmount}</TableCell>
-                    <TableCell align="right">{d.lender}</TableCell>
-                    <TableCell align="right">{d.borrower}</TableCell>
-                    <TableCell align="right">{d.isActive.toString()}</TableCell>
-                    <TableCell align="right">{d.unsettledAmount}</TableCell>
-                    <TableCell align="right">{d.defaultAmount}</TableCell>
-                    <TableCell align="right">{d.lastUpdated}</TableCell>
-                </TableRow>)}
+                {loanData.length === 0 ?
+                    <TableRow>
+                        <Box sx={{ width: "100%", height: "50px", display: "flex", justifyContent: "center", alignItems: "center" }}>No data to show</Box>
+                    </TableRow> :
+                    loanData.map((d: any, i: number) => <TableRow
+                        key={i}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                        <TableCell>{d.loanId}</TableCell>
+                        <TableCell align="right">{d.type}</TableCell>
+                        <TableCell align="right">{d.desc}</TableCell>
+                        <TableCell align="right">{d.creationTime}</TableCell>
+                        <TableCell align="right">{d.sanctionedAmount}</TableCell>
+                        <TableCell align="right">{d.lender}</TableCell>
+                        <TableCell align="right">{d.borrower}</TableCell>
+                        <TableCell align="right">{d.isActive.toString()}</TableCell>
+                        <TableCell align="right">{d.unsettledAmount}</TableCell>
+                        <TableCell align="right">{d.defaultAmount}</TableCell>
+                        <TableCell align="right">{d.lastUpdated}</TableCell>
+                    </TableRow>)}
             </TableBody>
         </Table>
     </TableContainer>
