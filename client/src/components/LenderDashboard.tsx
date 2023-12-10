45fa2fbff72c64da7226550606484f6170f962c9
Sameer Kushwaha <sameerkushwaha@Sameers-MacBook-Air.local>
2023-12-10 06:16:10 +0530

    call lending registration

--- client/src/components/LenderDashboard.tsx
+++ client/src/components/LenderDashboard.tsx
@@ -4,7 +4,7 @@ import Typography from "@mui/material/Typography";
 import {FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField} from "@mui/material";
 import Button from "@mui/material/Button";
 import * as React from "react";
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {BorrowerStats, LoanData, useApiContext} from "../contextProviders/APIContextProvider";
 import {useGlobalContext} from "../contextProviders/GlobalContextProvider";
 
@@ -14,16 +14,16 @@ export const LenderDashboard = () => {
     const {account} = useGlobalContext();
     const {registerLoan} = useApiContext();
 
-    const [lendingFormData, setLendingFormData] = useState({});
-
-    useEffect(() => {
-
-    }, []);
-
-    const lendingFormDataTmp = {loanType: 0, description: '', amount: 0, borrowerAddress: ''}
+    const lendingFormData = useRef({loanType: 0, description: '', amount: 0, borrowerAddress: ''})
 
     const handleLendingRegistration = () => {
-        setLendingFormData(lendingFormDataTmp);
+        console.log(lendingFormData.current)
+        registerLoan(lendingFormData.current.loanType, lendingFormData.current.description,
+            lendingFormData.current.amount, lendingFormData.current.borrowerAddress).then(res => {
+                if(res.isError){
+                    alert(res.message);
+                }
+        })
     }
 
     return <>
@@ -38,15 +38,15 @@ export const LenderDashboard = () => {
                     defaultValue={0}
                     name="loan-type-radio-buttons-group"
                     sx={{display: "flex", flexDirection: "row"}}
-                    onChange={(e)=>{lendingFormDataTmp.loanType = parseInt(e.target.value)}}
+                    onChange={(e)=>{lendingFormData.current.loanType = parseInt(e.target.value)}}
                 >
                     <FormControlLabel value={0} control={<Radio />} label="Credit Line" />
                     <FormControlLabel value={1} control={<Radio />} label="Consumer Loan" />
                 </RadioGroup>
             </FormControl>
-            <TextField id="outlined-basic" label="Description" variant="outlined" onChange={(e) => {lendingFormDataTmp.description = e.target.value}}/>
-            <TextField id="outlined-basic" label="Amount" variant="outlined" onChange={(e) => {lendingFormDataTmp.amount = parseInt(e.target.value)}}/>
-            <TextField id="outlined-basic" label="Borrower's Account" variant="outlined" onChange={(e) => {lendingFormDataTmp.borrowerAddress = e.target.value}}/>
+            <TextField id="outlined-basic" label="Description" variant="outlined" onChange={(e) => {lendingFormData.current.description = e.target.value}}/>
+            <TextField id="outlined-basic" label="Amount" variant="outlined" onChange={(e) => {lendingFormData.current.amount = parseInt(e.target.value)}}/>
+            <TextField id="outlined-basic" label="Borrower's Account" variant="outlined" onChange={(e) => {lendingFormData.current.borrowerAddress = e.target.value}}/>
             <Button color="success" variant="outlined" onClick={handleLendingRegistration}>Register the lending</Button>
         </Grid2>
 
